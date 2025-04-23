6ce2ad83afa867ceea8f28c9b691cc35a4cfe973
Danny Prikazsky <dannyprikaz@gmail.com>
2025-04-23 10:25:08 -0700

    Basic landing screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { Kanit_700Bold, useFonts } from "@expo-google-fonts/kanit";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded, error] = useFonts({
+    Kanit_700Bold,
+  })
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+  
+  return <Stack
+    screenOptions={{
+      headerShown: false,
+    }}
+   />;
 }
